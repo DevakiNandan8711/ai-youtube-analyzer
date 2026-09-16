3c838f6e493438fb3739f46d000ee48093b9d8bc
DevakiNandan8711 <devakinandan076@gmail.com>
2026-09-16 18:23:37 +0530

    Fix sys.stdout buffer error for Streamlit deployment

--- youtube_analyzer.py
+++ youtube_analyzer.py
@@ -1,7 +1,10 @@
 import sys
 import io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', errors='replace')
-sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8', errors='replace')
+
+if hasattr(sys.stdout, 'buffer'):
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', errors='replace')
+if hasattr(sys.stderr, 'buffer'):
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8', errors='replace')
 
 
 from textwrap import dedent
